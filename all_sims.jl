e1842cb8979e053aa01d62f89dca2fe0d1edc149
Gabriel Colon-Reyes <geclonr@berkeley.edu>
2023-07-26 13:52:48 -0700

    utils.jl to packagize

--- all_sims.jl
+++ all_sims.jl
@@ -28,16 +28,16 @@ function choose_disturbance(sys, dist)
     if dist == "CRC"
         # Control reference change
         g = get_component(DynamicInverter, sys, "generator-102-1")
-        crc = ControlReferenceChange(1.0, g, :V_ref, 0.95)
+        crc = ControlReferenceChange(0.25, g, :V_ref, 0.95)
     elseif dist == "NetworkSwitch"
         # NetworkSwitch
         yb = Ybus(sys).data
         new_yb = yb/1.0
-        ns = NetworkSwitch(1.0, new_yb)
+        ns = NetworkSwitch(0.25, new_yb)
     elseif dist == "InfBusChange"
         # Source bus voltage change
         s_device = first(get_components(Source, sys))
-        s_change = SourceBusVoltageChange(1.0, s_device, :V_ref, 1.048)
+        s_change = SourceBusVoltageChange(0.25, s_device, :V_ref, 1.048)
     else
         return error("Unknown disturbance")
     end
@@ -152,8 +152,6 @@ function build_seg_model(sys_segs, p::ExpParams)
     return sys_segs
 end
 
-
-
 function run_experiment(file_name, t_max, dist, line_model, p::ExpParams)
     # build system
     sys = System(joinpath(pwd(), file_name));
@@ -198,7 +196,7 @@ function run_experiment(file_name, t_max, dist, line_model, p::ExpParams)
     exec = execute_sim(sim, p)
     # read results
     results = results_sim(sim)
-    return results
+    return results, sys
 end
 
 file_name = "test_sys.json"
@@ -214,26 +212,36 @@ r_km = 0.05 # Ω/km
 x_km = 0.488 # Ω/km
 g_km = 0 # S/km
 b_km = 3.371e-6 # S/km
-l = 100 #km
-N = 10
+l = 1000 #km
+N = 1
 abstol = 1e-13
 reltol = 1e-10
 maxiters = Int(1e10)
 p = ExpParams(N, l, Z_c, r_km, x_km, g_km, b_km, abstol, reltol, maxiters)
 
 line_model_1 = "Algebraic"
-results_alg = run_experiment(file_name, t_max, dist, line_model_1, p)
+results_alg, sys = run_experiment(file_name, t_max, dist, line_model_1, p);
 
 line_model_2 = "Dynamic"
-results_dyn = run_experiment(file_name, t_max, dist, line_model_2, p)
-
-line_model_3 = "Multi-Segment Dynamic"
-results_ms_dyn = run_experiment(file_name, t_max, dist, line_model_3, p)
+results_dyn, dyn_sys = run_experiment(file_name, t_max, dist, line_model_2, p);
 
 vr_alg = get_state_series(results_alg, ("generator-102-1", :vr_filter));
+# sim_time, vr_alg = get_voltage_magnitude_series(results_alg, 1)
 plot(vr_alg, xlabel = "time", ylabel = "vr p.u.", label = "vr")
 vr_dyn = get_state_series(results_dyn, ("generator-102-1", :vr_filter));
-plot!(vr_dyn, xlabel = "time", ylabel = "vr", label = "vr_dyn")
-vr_ms_dyn = get_state_series(results_ms_dyn, ("generator-102-1", :vr_filter));
-plot!(vr_ms_dyn, xlabel = "time", ylabel = "vr p.u.", label = "vr_segs_$(N)", xlims=(0.999, 1.01))
-plot!(xlims=(0.999, 1.02))
+plot!(vr_dyn, xlabel = "time", ylabel = "vr p.u.", label = "vr_dyn")
+
+line_model_3 = "Multi-Segment Dynamic"
+
+for N in [1]
+    print(N)
+    p = ExpParams(N, l, Z_c, r_km, x_km, g_km, b_km, abstol, reltol, maxiters)
+    
+    results_ms_dyn, seg_sys = nothing, nothing
+    results_ms_dyn, seg_sys = run_experiment(file_name, t_max, dist, line_model_3, p)
+
+    vr_ms_dyn = get_state_series(results_ms_dyn, ("generator-102-1", :vr_filter));
+    display(plot!(vr_ms_dyn, xlabel = "time", ylabel = "vr p.u.", label = "vr_segs_$(N)"))
+end
+
+plot!(xlims=(0, 1))
--- utils.jl
+++ utils.jl
@@ -1,5 +1,5 @@
-using Pkg;
-Pkg.activate(".")
+module EffectsOfTLDynamics
+
 using PowerSystems
 using PowerSimulationsDynamics
 using Sundials
@@ -8,21 +8,9 @@ using PowerNetworkMatrices
 using SparseArrays
 using OrdinaryDiffEq
 
+
 const PSY = PowerSystems;
 const PSID = PowerSimulationsDynamics;
-
-struct ExpParams
-    N::Int
-    l::Float64
-    Z_c::Float64
-    r_km::Float64
-    x_km::Float64
-    g_km::Float64
-    b_km::Float64
-    abstol::Float64
-    reltol::Float64
-    maxiters::Int
-end
 
 function choose_disturbance(sys, dist)
     if dist == "CRC"
@@ -228,53 +216,4 @@ function run_experiment(file_name, t_max, dist, line_model, p::ExpParams)
     return results, sim
 end
 
-file_name = "threebus_sys.json"
-t_max = 2.0
-
-# "CRC"
-# "NetworkSwitch"
-# "InfBusChange"
-dist = "CRC"
-
-Z_c = 380 # Ω
-r_km = 0.05 # Ω/km
-x_km = 0.488 # Ω/km
-g_km = 0 # S/km
-b_km = 3.371e-6 # S/km
-l = 100 #km
-N = 10
-abstol = 1e-13
-reltol = 1e-10
-maxiters = Int(1e10)
-p = ExpParams(N, l, Z_c, r_km, x_km, g_km, b_km, abstol, reltol, maxiters)
-
-line_model_1 = "Algebraic"
-results_alg, sim = run_experiment(file_name, t_max, dist, line_model_1, p);
-
-line_model_2 = "Dynamic"
-results_dyn, dyn_sim = run_experiment(file_name, t_max, dist, line_model_2, p);
-
-line_model_3 = "Multi-Segment Dynamic"
-results_ms_dyn, seg_sim = run_experiment(file_name, t_max, dist, line_model_3, p);
-
-vr_alg = get_state_series(results_alg, ("BUS 1", :Vm));
-plot(vr_alg, xlabel = "time", ylabel = "vr p.u.", label = "vr")
-vr_dyn = get_state_series(results_dyn, ("generator-103-1", :vr_filter));
-plot!(vr_dyn, xlabel = "time", ylabel = "vr", label = "vr_dyn")
-vr_ms_dyn = get_state_series(results_ms_dyn, ("generator-103-1", :vr_filter));
-plot!(vr_ms_dyn, xlabel = "time", ylabel = "vr p.u.", label = "vr_segs_$(N)", xlims=(0.999, 1.01))
-plot!(xlims=(0.999, 1.02))
-
-
-sys = System(joinpath(pwd(), file_name));
-tspan = (0.0, t_max)
-perturbation = choose_disturbance(sys, "CRC")
-sys = build_new_impedance_model(sys, p)
-sim = build_sim(sys, tspan, perturbation, false)
-x = small_signal_analysis(sim)
-
-dsim = build_sim(sys, tspan, perturbation, true)
-
-dx = small_signal_analysis(dsim)
-
-exec = execute_sim(dsim, p) 
+end
