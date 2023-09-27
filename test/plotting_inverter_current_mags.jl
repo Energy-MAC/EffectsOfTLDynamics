1b7b0cb3a8c779fd2df1d8413733c69dc39b2ad6
gecolonr <gecolonr@berkeley.edu>
2023-09-27 01:38:15 +0000

    plots

--- test/plotting_inverter_current_mags.jl
+++ test/plotting_inverter_current_mags.jl
@@ -14,7 +14,7 @@ using Plots
 const PSY = PowerSystems;
 const PSID = PowerSimulationsDynamics;
 
-rn = "2023-09-27T01:07:39.795" #string date
+rn = "2023-09-27T01:29:18.988" #string date
 model = "GFL_v_machine"
 main_path = "../results/"*model*"/"*rn
 
@@ -28,8 +28,8 @@ sol_dyn = nothing
 sol_ms = nothing
 sol_msmb = nothing
 
-line_scales = collect(1.00)
-load_scales = collect(0.5:0.5:3.0)
+line_scales = collect(3.00)
+load_scales = collect(0.5)
 
 line_scales = collect(1.0)
 load_scales = collect(1.0)
@@ -45,16 +45,16 @@ for var_to_measure in vars_to_measure
             
             partial_path = main_path*"/$(line_scale)_$(load_scale)"
             folder_path = partial_path*"/statpi"
-            df_alg = CSV.read(partial_path*"/statpi/"*var_to_measure, DataFrame);
+            # df_alg = CSV.read(partial_path*"/statpi/"*var_to_measure, DataFrame);
             df_dyn = CSV.read(partial_path*"/dynpi/"*var_to_measure, DataFrame); 
             df_ms = CSV.read(partial_path*"/MSSB/"*var_to_measure, DataFrame);
             df_msmb = CSV.read(partial_path*"/MSMB/"*var_to_measure, DataFrame);
-            sol_alg = (df_alg."Time", df_alg."generator-102-1_i_mag")
+            # sol_alg = (df_alg."Time", df_alg."generator-102-1_i_mag")
             sol_dyn = (df_dyn."Time", df_dyn."generator-102-1_i_mag")
             sol_ms = (df_ms."Time", df_ms."generator-102-1_i_mag")
             sol_msmb = (df_msmb."Time", df_msmb."generator-102-1_i_mag")
         
-            plt = plot(sol_alg, label = L"\mathrm{statpi}")
+            # plt = plot(sol_alg, label = L"\mathrm{statpi}")
             plot!(plt, sol_dyn, label = L"\mathrm{dynpi}")
             plot!(plt, sol_ms, label = L"\mathrm{MSSB}")
             plot!(plt, sol_msmb, label = L"\mathrm{MSMB}")
