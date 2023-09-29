388c27758039955a9c6112196b9537f98bb4578c
gecolonr <gecolonr@berkeley.edu>
2023-09-29 22:42:58 +0000

    ivM

--- test/plotting_inverter_current_mags.jl
+++ test/plotting_inverter_current_mags.jl
@@ -15,8 +15,8 @@ const PSY = PowerSystems;
 const PSID = PowerSimulationsDynamics;
 
 
-rn = "2023-09-29T18:40:00.713" #string date
-model = "9bus_slackless"
+rn = "2023-09-24T20:12:13.348" #string date
+model = "inv_v_machine"
 main_path = "../results/"*model*"/"*rn
 
 df_alg = nothing
@@ -30,7 +30,7 @@ sol_ms = nothing
 sol_msmb = nothing
 
 
-line_scales = collect(1.0:2.0:3.0)
+line_scales = collect(1.0:4.0:5.0)
 load_scales = collect(0.5:0.5:1.0)
 
 vars_to_measure = ["inverter_currents.csv"]
@@ -38,8 +38,8 @@ vars_to_measure = ["inverter_currents.csv"]
 plots = []
 plt = []
 
-line_scales = 1.0
-load_scales = [0.5]
+line_scales = [1.0 5.0]
+load_scales = [0.5 1.0 2.0]
 
 for var_to_measure in vars_to_measure
     for line_scale in line_scales
@@ -51,10 +51,10 @@ for var_to_measure in vars_to_measure
             df_dyn = CSV.read(partial_path*"/dynpi/"*var_to_measure, DataFrame); 
             df_ms = CSV.read(partial_path*"/MSSB/"*var_to_measure, DataFrame);
             df_msmb = CSV.read(partial_path*"/MSMB/"*var_to_measure, DataFrame);
-            sol_alg = (df_alg."Time", df_alg."generator-3-1_i_mag")
-            sol_dyn = (df_dyn."Time", df_dyn."generator-3-1_i_mag")
-            sol_ms = (df_ms."Time", df_ms."generator-3-1_i_mag")
-            sol_msmb = (df_msmb."Time", df_msmb."generator-3-1_i_mag")
+            sol_alg = (df_alg."Time", df_alg."generator-102-1_i_mag")
+            sol_dyn = (df_dyn."Time", df_dyn."generator-102-1_i_mag")
+            sol_ms = (df_ms."Time", df_ms."generator-102-1_i_mag")
+            sol_msmb = (df_msmb."Time", df_msmb."generator-102-1_i_mag")
         
             plt = plot(sol_alg, label = L"\mathrm{statpi}", legend = :bottomright)
             plot!(plt, sol_dyn, label = L"\mathrm{dynpi}")
@@ -67,10 +67,10 @@ for var_to_measure in vars_to_measure
             plot!(size=(1000,800))
             plot!(xguidefontsize=25, yguidefontsize=25, tickfontsize=16,legendfontsize=16)
             
-            savefig(partial_path*"/"*var_to_measure[1:end-4]*"_mags3.svg")
+            savefig(partial_path*"/"*var_to_measure[1:end-4]*"_mags.svg")
             plot!(ylims = ())
             plot!(xlims=(0.249, 0.275))
-            savefig(partial_path*"/"*var_to_measure[1:end-4]*"_mags_100ms_zoom3.svg")
+            savefig(partial_path*"/"*var_to_measure[1:end-4]*"_mags_100ms_zoom.svg")
             # push!(plots, plt)
         end 
     end
